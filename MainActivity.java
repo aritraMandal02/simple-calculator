af5d8adb6ffc1fa944082bff24fdae80f531ca9f
aritraMandal02 <87606712+aritraMandal02@users.noreply.github.com>
2021-07-22 16:33:37 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,309 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText calculation, result;
+    private String curr, res;
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDel, btnAC, btnDivide, btnMultiply, btnAddition, btnMinus, btnEqual, btnDecimal;
+    private boolean decimal_inserted, operator_inserted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calculation = (EditText) findViewById(R.id.calculation);
+        result = (EditText) findViewById(R.id.result);
+
+        curr = "";
+        res = "";
+        decimal_inserted = false;
+        operator_inserted = false;
+
+        btn0 = (Button) findViewById(R.id.btn0);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn9 = (Button) findViewById(R.id.btn9);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnDecimal = (Button) findViewById(R.id.btnDecimal);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnAC = (Button) findViewById(R.id.btnAC);
+        btnMinus = (Button) findViewById(R.id.btnMinus);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnEqual = (Button) findViewById(R.id.btnEqual);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "0";
+                displayOne();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "1";
+                displayOne();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "2";
+                displayOne();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "3";
+                displayOne();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "4";
+                displayOne();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "5";
+                displayOne();
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "6";
+                displayOne();
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "7";
+                displayOne();
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "8";
+                displayOne();
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curr = curr + "9";
+                displayOne();
+            }
+        });
+
+        btnDecimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(curr.isEmpty()){
+                    curr = "0.";
+                    decimal_inserted = true;
+                }
+                if(decimal_inserted == false){
+                    curr = curr + ".";
+                    decimal_inserted = true;
+                }
+                displayOne();
+            }
+        });
+
+        btnAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clear();
+                displayOne();
+                displayTwo();
+            }
+        });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backspace();
+                displayOne();
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimal_inserted = false;
+
+                if(!curr.isEmpty()){
+                    if(curr.substring(curr.length()-1, curr.length()).equals(".")){
+                        backspace();
+                    }
+                    if(operator_inserted == false){
+                        curr = curr + " ÷ ";
+                        operator_inserted = true;
+                    }
+                }
+                displayOne();
+            }
+        });
+
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimal_inserted = false;
+
+                if(!curr.isEmpty()){
+                    if(curr.substring(curr.length()-1, curr.length()).equals(".")){
+                        backspace();
+                    }
+                    if(operator_inserted == false){
+                        curr = curr + " ✕ ";
+                        operator_inserted = true;
+                    }
+                }
+                displayOne();
+            }
+        });
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimal_inserted = false;
+
+                if(!curr.isEmpty()){
+                    if(curr.substring(curr.length()-1, curr.length()).equals(".")){
+                        backspace();
+                    }
+                    if(operator_inserted == false){
+                        curr = curr + " + ";
+                        operator_inserted = true;
+                    }
+                }
+                displayOne();
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimal_inserted = false;
+
+                if(!curr.isEmpty()){
+                    if(curr.substring(curr.length()-1, curr.length()).equals(".")){
+                        backspace();
+                    }
+                    if(operator_inserted == false){
+                        curr = curr + " - ";
+                        operator_inserted = true;
+                    }
+                }
+                displayOne();
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decimal_inserted = false;
+
+                if(!curr.isEmpty()){
+                    if(curr.substring(curr.length()-1, curr.length()).equals(".")){
+                        backspace();
+                    }
+                    if(operator_inserted == false){
+                        curr = curr + " ÷ ";
+                        operator_inserted = true;
+                    }
+                }
+                displayOne();
+            }
+        });
+
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(operator_inserted == true && !curr.substring(curr.length()-1, curr.length()).equals(" ")){
+
+                    String [] tokens = curr.split( " ");
+
+                    switch (tokens[1].charAt(0)){
+                        case '+':
+                            res = Double.toString( Double.parseDouble(tokens[0]) + Double.parseDouble(tokens[2]));
+                            break;
+                        case '-':
+                            res = Double.toString( Double.parseDouble(tokens[0]) - Double.parseDouble(tokens[2]));
+                            break;
+                        case '÷':
+                            res = Double.toString( Double.parseDouble(tokens[0]) / Double.parseDouble(tokens[2]));
+                            break;
+                        case '✕':
+                            res = Double.toString( Double.parseDouble(tokens[0]) * Double.parseDouble(tokens[2]));
+                            break;
+                    }
+                    displayTwo();
+                }
+            }
+        });
+
+
+    }
+
+    public void displayOne(){
+
+        calculation.setText(curr);
+    }
+
+    public void displayTwo(){
+
+        result.setText(res);
+    }
+
+    public void clear(){
+        curr = "";
+        res = "";
+        decimal_inserted = false;
+        operator_inserted = false;
+    }
+    public void backspace(){
+        if(!curr.isEmpty()) {
+            if (curr.substring(curr.length() - 1, curr.length()).equals(".")) {
+                decimal_inserted = false;
+            }
+            if(curr.substring(curr.length()-1, curr.length()).equals(" ")){
+                curr = curr.substring(0, curr.length()-3);
+                operator_inserted = false;
+            }else{
+                curr = curr.substring(0, curr.length()-1);
+            }
+        }
+    }
+}
